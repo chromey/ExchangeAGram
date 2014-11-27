8cb330376ddd2f97869c75a857efb94f7550b711
Christian Romeyke <christian@romeyke.de>
2014-11-28 00:14:09 +0100

    first couple of lessons
    up to and including “FeedCell”

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Christian Romeyke on 28/11/14.
+//  Copyright (c) 2014 Christian Romeyke. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
---
+++ FeedViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FeedViewController.swift
+//  ExchangeAGram
+//
+//  Created by Christian Romeyke on 27/11/14.
+//  Copyright (c) 2014 Christian Romeyke. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // UICollectionViewDataSource
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+
+}
