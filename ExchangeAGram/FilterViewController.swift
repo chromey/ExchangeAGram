bca3a137401551c7d342692d2752e308d141d275
Christian Romeyke <christian@romeyke.de>
2014-12-05 00:54:36 +0100

    Finishing UIAlertController

--- ExchangeAGram/FilterViewController.swift
+++ ExchangeAGram/FilterViewController.swift
@@ -133,6 +133,24 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
             textField.placeholder = "Add Caption!"
             textField.secureTextEntry = false
         }
+        var text: String
+        let textField = alert.textFields![0] as UITextField
+        if textField.text != nil {
+            text = textField.text
+        }
+        let photoAction = UIAlertAction(title: "Post Photo to Facebook with Caption", style: UIAlertActionStyle.Destructive) { (uiAlertAction) -> Void in
+            
+        }
+        alert.addAction(photoAction)
+        let saveFilterAction = UIAlertAction(title: "Save Filter without posting to Facebook", style: UIAlertActionStyle.Default) { (uiAlertAction) -> Void in
+            
+        }
+        alert.addAction(saveFilterAction)
+        let cancelAction = UIAlertAction(title: "Select another Filter", style: UIAlertActionStyle.Cancel) { (uiAlertAction) -> Void in
+            
+        }
+        alert.addAction(cancelAction)
+        
         self.presentViewController(alert, animated: true, completion: nil)
     }
     
