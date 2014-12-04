2b329e431b551f87590e15873d7f94a63ddd6d3c
Christian Romeyke <christian@romeyke.de>
2014-12-04 21:33:06 +0100

    Bridging Header
    for Objective C

---
+++ ExchangeAGram/BridgingHeader.h
@@ -0,0 +1,12 @@
+//
+//  BridgingHeader.h
+//  ExchangeAGram
+//
+//  Created by Christian Romeyke on 04/12/14.
+//  Copyright (c) 2014 Christian Romeyke. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
+
+
+#endif
